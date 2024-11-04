85346b2d6e8db91425417c9f9d4ad43c9119860f
Jia Hao Zhang <j.h.zhang@student.rug.nl>
2024-11-04 12:29:41 +0100

    added minimun scod fro deployment code to get started on actuall implemening the use of the page

---
+++ app/pages/3_🚀_Deployment.py
@@ -0,0 +1,14 @@
+from typing import TYPE_CHECKING
+from app.core.system import AutoMLSystem
+import streamlit as st
+
+if TYPE_CHECKING:
+    from autoop.core.ml.artifact import Artifact
+
+automl: AutoMLSystem = AutoMLSystem.get_instance
+
+st.set_page_config(page_title="Deployment")
+
+st.title("Deployment")
+
+piplines: list["Artifact"] = automl.registry.list()
